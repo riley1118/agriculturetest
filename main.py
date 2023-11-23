e8ae0d1b240c9c0b6f9cc6392c635150a0572756
riley1118 <rbakken02@hamline.edu>
2023-11-23 01:31:57 -0600

    Add main.py script

---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+import matplotlib as plt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
